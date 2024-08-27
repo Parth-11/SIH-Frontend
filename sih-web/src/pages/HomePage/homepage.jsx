8e497627908d02228f7584a20664a220cd6db2bd
Parth-11 <parth12.mgg@gmail.com>
2024-08-27 15:54:41 +0530

    Added empty files and a template for homepage

---
+++ sih-web/src/pages/HomePage/homepage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './homepage.module.css';
+
+const HomePage = () =>{
+    return(
+        <div>
+            <div></div>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            <footer></footer>
+        </div>
+    )
+}
+
+export default HomePage
