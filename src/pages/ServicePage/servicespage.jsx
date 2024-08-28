1dc96e01dd374216a8f3f5c5cf181023ace995e5
bhumika2856 <bhumikakundra28@gmail.com>
2024-08-28 09:30:24 +0530

    file created

---
+++ src/pages/ServicePage/servicespage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './Services.module.css';
+
+const ServicePage = () => {
+    return(
+        <div>
+
+        </div>
+    )
+
+}
+
+
+export default ServicePage
