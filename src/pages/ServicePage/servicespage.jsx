d3fcf1607b2b982ff41eaaa3893c695b8bbd3c48
bhumika2856 <bhumikakundra28@gmail.com>
2024-08-28 13:05:59 +0530

    file created

--- src/pages/ServicePage/servicespage.jsx
+++ src/pages/ServicePage/servicespage.jsx
@@ -2,10 +2,34 @@ import React from "react";
 import styles from './Services.module.css';
 
 const ServicePage = () => {
+
+    const services=[
+        {
+            image: '', 
+            title: 'Comprehensive Legal Database',
+            description: 'Lorem ipsum dolor sit amet.'
+
+        },
+
+        {
+            image: '', 
+            title: 'Comprehensive Legal Database',
+            description: 'Lorem ipsum dolor sit amet.'
+
+        },
+
+        {
+            image: '', 
+            title: 'Comprehensive Legal Database',
+            description: 'Lorem ipsum dolor sit amet.'
+
+        }
+    ]
+    
     return(
         <div>
             <div>
-                
+
             </div>
 
         </div>
